f1c739dfe487ed05c7074a1c966fa53c3cf9300a
Rodrigo Silva <rodrigowstudy@gmail.com>
2022-06-11 16:12:46 -0300

    Novo layout

--- Dev-U_RPG_Maker_Jam/js/plugins.js
+++ Dev-U_RPG_Maker_Jam/js/plugins.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8bd406e8273cbf6f3d24fbb3bd3e43ee301bbc54ac96652f649462839003dca
-size 6492
+oid sha256:cd41ac7a72c402214b9d86689b9906833968ac0ba638a784e8607ee5fcf97f87
+size 6491
